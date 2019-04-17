2f48f08c90181e314b9bdd57ac24f436b3a1dfec
Daniel <danielflorestamayo@gmail.com>
2019-04-17 18:23:37 -0500

    ejercicio2

--- ejercicio2/index.php
+++ ejercicio2/index.php
@@ -4,4 +4,19 @@
  *
  * Implementar una función que muestre un título centrado en pantalla, y la llamaremos posteriormente dos veces.
 */
+
+
+function imprimir(){
+    #Titulo impreso dibujando un cuadro etiqueta "div"
+    echo '<div align ="center">Centrar texto</div><br>';
+    #Titulo impreso con la etiqueta "h1"
+    echo ('<h1 align="center" >Rugby ready..!!</h1>');
+    
+return 0;
+}
+
+
+imprimir();
+imprimir();
+
 ?>
